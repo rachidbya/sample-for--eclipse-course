28de247c343fa0dd70cae751dec6127e01f32e4c
rachid <lihiarachid1971@gmail.com>
2019-01-07 16:41:06 +0100

    Add getter and setter

--- src/org/bya/model/User.java
+++ src/org/bya/model/User.java
@@ -3,4 +3,12 @@ package org.bya.model;
 
 public class User {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
 }
